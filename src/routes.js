c0abd245d735ac61a3aea1a41fccf6c6872437f5
Monick Évelyn <monick.evelyn.tech@gmail.com>
2025-11-11 11:44:12 -0300

    rota GET/tasks para listar todas as tarefas

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,33 @@
 import { buildRoutePath } from "./utils/build-route-path.js"
 import {randomUUID} from 'node:crypto'
 
+const tasks = []
 export const routes = [
     {
         method: 'POST',
         path: buildRoutePath('/tasks'),
         handler: (req, res) => {
             const {title, description} = req.body;
-            const task = {
+            tasks.push({
                 id: randomUUID(),
                 title,
                 description,
                 completed_at: null,
                 created_at: new Date(),
                 updated_at: new Date()
-            }
-            console.log(task);
-            return res.writeHead(201).end();
+            })
+            console.log(tasks);
+            //tasks.push(task);
+            return res.writeHead(201).end('Tarefa criada com sucesso!');
+        }
+    },
+    {
+        method: 'GET',
+        path: buildRoutePath('/tasks'),
+        handler: (req, res) => {
+            return res
+                .setHeader('Content-type', 'aplication/json')
+                .end(JSON.stringify(tasks))
         }
     },
 ]
